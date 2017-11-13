96af3e6220c5b17a7b2e34f9cdf8d0b6dc4884cf
shuajia <819418850@qq.com>
2017-11-13 10:38:40 +0800

    jia

--- app/src/main/java/com/jia/notetext/NormalActivity.java
+++ app/src/main/java/com/jia/notetext/NormalActivity.java
@@ -1,13 +1,27 @@
 package com.jia.notetext;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class NormalActivity extends AppCompatActivity {
+import com.jia.jsnotetext.JsNoteTextView;
+
+public class NormalActivity extends Activity {
+
+    private String text="    月光如流水一般，静静地泻在这一片叶子和花上。薄薄的青雾浮起在荷塘里。叶子和花仿佛在牛乳中洗过一样；又像笼着轻纱的梦。虽然是满月，天上却有一层淡淡的云，所以不能朗照；但我以为这恰是到了好处——酣眠固不可少，小睡也别有风味的。月光是隔了树照过来的，高处丛生的灌木，落下参差的斑驳的黑影，峭楞楞如鬼一般；弯弯的杨柳的稀疏的倩影，却又像是画在荷叶上。塘中的月色并不均匀；但光与影有着和谐的旋律，如梵婀上奏着的名曲。\n"+
+            "    荷塘的四面，远远近近，高高低低都是树，而杨柳最多。这些树将一片荷塘重重围住；只在小路一旁，漏着几段空隙，像是特为月光留下的。树色一例是阴阴的，乍看像一团烟雾；但杨柳的丰姿，便在烟雾里也辨得出。树梢上隐隐约约的是一带远山，只有些大意罢了。树缝里也漏着一两点路灯光，没精打采的，是渴睡人的眼。这时候最热闹的，要数树上的蝉声与水里的蛙声；但热闹是它们的，我什么也没有。";
+
+    private JsNoteTextView tv_normal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normal);
+
+        tv_normal= findViewById(R.id.tv_normal);
+
+        tv_normal.setText(text);
+
+        tv_normal.setNoteIndex(20,180);
     }
 }
